00eb42c1418c638064254683fc5d3830610bdb21
Suriyakanth.S <93136671+suriyakanth1@users.noreply.github.com>
2024-06-13 13:53:45 +0530

    Added project

---
+++ mavenselenium/src/test/java/Build/mavenselenium/AppTest.java
@@ -0,0 +1,58 @@
+package Build.mavenselenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        // Set the path to the ChromeDriver executable
+        // System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+
+        // Create a new instance of the ChromeDriver
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void testExample() {
+        // Navigate to Google
+        driver.get("https://www.google.co.in");
+
+        // Print success message
+        System.out.println("Browser opened successfully.");
+
+        // Enter search query and press Enter
+        driver.findElement(By.name("q")).sendKeys("Selenium", Keys.ENTER);
+        System.out.println("Selenium search.");
+
+        // Click on a link containing the word "selenium"
+        driver.findElement(By.partialLinkText("selenium")).click();
+        System.out.println("Navigated to Selenium page.");
+
+        // Click on the "Download" link
+        driver.findElement(By.partialLinkText("Download")).click();
+        System.out.println("Clicked on Download link.");
+
+        // Get the title of the page
+        String pageTitle = driver.getTitle();
+
+        // Assert the title
+        assertEquals("Downloads | Selenium", pageTitle);
+    }
+
+    @After
+    public void tearDown() {
+        // Close the browser
+        driver.quit();
+        System.out.println("Browser quit successfully.");
+    }
+}
